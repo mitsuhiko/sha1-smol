a17af99f01c401ed6f10e738de3fbd9a8d804b53
Armin Ronacher <armin.ronacher@active-4.com>
2014-11-21 13:56:28 +0100

    Initial version.

---
+++ src/lib.rs
@@ -0,0 +1,199 @@
+//! A minimal implementation of SHA1 for rust.
+//!
+//! Example:
+//!
+//! ```rust
+//! extern crate sha1;
+//! # fn main() {
+//!
+//! let mut m = sha1::Sha1::new();
+//! m.update("Hello World!".as_bytes());
+//! assert_eq!(m.hexdigest().as_slice(),
+//!            "2ef7bde608ce5404e97d5f042f95f89f1c232871");
+//! # }
+//! ```
+
+#![license = "BSD"]
+#![comment = "Minimal implementation of SHA1 for Rust."]
+
+#![feature(slicing_syntax)]
+
+#![experimental]
+
+extern crate serialize;
+
+use std::io::{MemWriter, BufWriter};
+
+
+/// Represents a Sha1 hash object in memory.
+#[deriving(Clone)]
+pub struct Sha1 {
+    state: [u32, ..5],
+    data: Vec<u8>,
+    len: u64,
+}
+
+const DEFAULT_STATE : [u32, ..5] =
+    [0x67452301, 0xefcdab89, 0x98badcfe, 0x10325476, 0xc3d2e1f0];
+
+
+fn to_hex(input: &[u8]) -> String {
+    let mut s = String::new();
+    for b in input.iter() {
+        s.push_str(format!("{:02x}", *b)[]);
+    }
+    return s;
+}
+
+
+impl Sha1 {
+
+    /// Creates an fresh sha1 hash object.
+    pub fn new() -> Sha1 {
+        Sha1 {
+            state: DEFAULT_STATE,
+            data: Vec::new(),
+            len: 0,
+        }
+    }
+
+    fn process_block(&mut self, block: &[u8]) {
+        assert_eq!(block.len(), 64);
+
+        let mut words = [0u32, ..80];
+        for (i, chunk) in block.chunks(4).enumerate() {
+            words[i] = (chunk[3] as u32) |
+                       (chunk[2] as u32 << 8) |
+                       (chunk[1] as u32 << 16) |
+                       (chunk[0] as u32 << 24);
+        }
+
+        let ff = |b: u32, c: u32, d: u32| d ^ (b & (c ^ d));
+        let gg = |b: u32, c: u32, d: u32| b ^ c ^ d;
+        let hh = |b: u32, c: u32, d: u32| (b & c) | (d & (b | c));
+        let ii = |b: u32, c: u32, d: u32| b ^ c ^ d;
+
+        let left_rotate = |x: u32, n: u32| (x << n as uint) | (x >> (32 - n) as uint);
+
+        for i in range(16u, 80u) {
+            let n = words[i - 3] ^ words[i - 8] ^ words[i - 14] ^ words[i - 16];
+            words[i] = left_rotate(n, 1);
+        }
+
+        let mut a = self.state[0];
+        let mut b = self.state[1];
+        let mut c = self.state[2];
+        let mut d = self.state[3];
+        let mut e = self.state[4];
+
+        for i in range(0u, 80u) {
+            let (f, k) = match i {
+                0 ... 19 => (ff(b, c, d), 0x5a827999),
+                20 ... 39 => (gg(b, c, d), 0x6ed9eba1),
+                40 ... 59 => (hh(b, c, d), 0x8f1bbcdc),
+                60 ... 79 => (ii(b, c, d), 0xca62c1d6),
+                _ => (0, 0),
+            };
+
+            let tmp = left_rotate(a, 5) + f + e + k + words[i];
+            e = d;
+            d = c;
+            c = left_rotate(b, 30);
+            b = a;
+            a = tmp;
+        }
+
+        self.state[0] += a;
+        self.state[1] += b;
+        self.state[2] += c;
+        self.state[3] += d;
+        self.state[4] += e;
+    }
+
+    /// Resets the hash object to it's initial state.
+    pub fn reset(&mut self) {
+        self.state = DEFAULT_STATE;
+        self.data.clear();
+        self.len = 0;
+    }
+
+    /// Update hash with input data.
+    pub fn update(&mut self, data: &[u8]) {
+        let mut d = self.data.clone();
+        self.data.clear();
+
+        d.push_all(data);
+
+        for chunk in d[].chunks(64) {
+            self.len += chunk.len() as u64;
+
+            if chunk.len() == 64 {
+                self.process_block(chunk);
+            } else {
+                self.data.push_all(chunk);
+            }
+        }
+    }
+
+    /// Retrieve digest result.  The output must be large enough to
+    /// contain result (20 bytes).
+    pub fn output(&self, out: &mut [u8]) {
+        // these are unlikely to fail, since we're writing to memory
+        #![allow(unused_must_use)]
+
+        let mut m = Sha1 {
+            state: self.state,
+            data: Vec::new(),
+            len: 0,
+        };
+
+        let mut w = MemWriter::new();
+        w.write(self.data[]);
+        w.write_u8(0x80 as u8);
+        w.write(Vec::from_elem(56 - self.data.len() - 1, 0x00 as u8)[]);
+        w.write_be_u64(self.len * 8);
+        m.process_block(w.get_ref());
+
+        let mut w = BufWriter::new(out);
+        for &n in m.state.iter() {
+            w.write_be_u32(n);
+        }
+    }
+
+    /// Shortcut for getting `output` into a new vector.
+    pub fn digest(&self) -> Vec<u8> {
+        let mut buf = Vec::from_elem(20, 0u8);
+        self.output(buf[mut]);
+        buf
+    }
+
+    /// Shortcut for getting a hex output of the vector.
+    pub fn hexdigest(&self) -> String {
+        to_hex(self.digest()[])
+    }
+}
+
+
+#[test]
+fn test_simple() {
+    let mut m = Sha1::new();
+
+    let tests = [
+        ("The quick brown fox jumps over the lazy dog",
+         "2fd4e1c67a2d28fced849ee1bb76e7391b93eb12"),
+        ("The quick brown fox jumps over the lazy cog",
+         "de9f2c7fd25e1b3afad3e85a0bd17d9b100db4b3"),
+        ("", "da39a3ee5e6b4b0d3255bfef95601890afd80709"),
+    ];
+
+    for &(s, ref h) in tests.iter() {
+        let data = s.as_bytes();
+
+        m.reset();
+        m.update(data);
+        let hh = m.hexdigest();
+
+        assert_eq!(hh.len(), h.len());
+        assert_eq!(hh[], *h);
+    }
+}
